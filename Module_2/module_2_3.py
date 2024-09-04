895fbd1359a8add6b3c38750b77080dea77f2d6c
Guru-13-nv <gurmaks@mail.ru>
2024-09-04 18:09:07 +0300

    Изменено имя переменной в соответствии с заданием

--- Module_2/module_2_3.py
+++ Module_2/module_2_3.py
@@ -11,23 +11,23 @@
 # Используйте операторы прерывания/продолжения цикла в соответствии с условиями задачи.
 from itertools import count
 
-list_number = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 number = 0
-# print (len(list_number))
-# print (list_number [1])
-while number <= len(list_number) - 1:  # Ограничение цикла, нумерация индексов начинается с нуля.
-    if list_number[number] > 0:  # Условие цикла, где 0 - не является положительным и не является отрицательным числом
-        print(list_number[number])
+# print (len(my_list))
+# print (my_list [1])
+while number <= len(my_list) - 1:  # Ограничение цикла, нумерация индексов начинается с нуля.
+    if my_list[number] > 0:  # Условие цикла, где 0 - не является положительным и не является отрицательным числом
+        print(my_list[number])
         number = number + 1
-    elif list_number[number] == 0: #Поскольку ноль не останавливает цикл и не выводится
+    elif my_list[number] == 0: #Поскольку ноль не останавливает цикл и не выводится
         number = number + 1
     else:
         print('Список закончился или найдено отрицательное число')
         break  # Остановка цикла при неудовлетворении условиям
 # Если выписать просто все положительные числа
-# while number <= len(list_number)-1:
-#     if list_number[number] > 0:
-#         print(list_number[number])
+# while number <= len(my_list)-1:
+#     if my_list[number] > 0:
+#         print(my_list[number])
 #         number = number + 1
 #     else:
 #         number = number + 1
