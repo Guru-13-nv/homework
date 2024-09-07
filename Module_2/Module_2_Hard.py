e36d6cb45e352a48bd1b64e3010fc06ad45972fa
Guru-13-nv <gurmaks@mail.ru>
2024-09-07 23:02:19 +0300

    Подгонка вывода под пример. В типе строки

--- Module_2/Module_2_Hard.py
+++ Module_2/Module_2_Hard.py
@@ -10,10 +10,11 @@ def funk_stone():  # Функция выбора случайного камня
     stone_random = random.choice(stone)  # Метод для рандомного выбора числа из переданного в него списка
     return stone_random
 
+
 random_1 = funk_stone()
-pas_final = []
+pas_final = ''
 for pas_1 in range(1, 20):
     for pas_2 in range(1, 20):
         if random_1 % (pas_1 + pas_2) == 0 and pas_1 < pas_2 and pas_1 != pas_2:
-            pas_final.append([pas_1, pas_2])
+            pas_final += str(pas_1) + str(pas_2)
 print(f'Выпал камень: {random_1}\n Пароль к нему {pas_final}')
