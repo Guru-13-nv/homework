09da943140f4f4dec4d783f75bbbf65e8c4208b6
Guru-13-nv <gurmaks@mail.ru>
2024-09-09 21:15:34 +0300

    Готовый вариант

---
+++ Module_3/module_3_1.py
@@ -0,0 +1,84 @@
+# module_3_1
+'''                                         Задача "Счётчик вызовов":
+Порой необходимо отслеживать, сколько раз вызывалась та или иная функция. К сожалению, в Python не предусмотрен подсчёт
+вызовов автоматически.
+Давайте реализуем данную фишку самостоятельно!
+
+Вам необходимо написать 3 функции:
+Функция count_calls подсчитывающая вызовы остальных функций.
+Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре,
+строку в нижнем регистре.
+Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке,
+False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+Пункты задачи:
+Создать переменную calls = 0 вне функций.
+Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух
+функциях.
+Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
+Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
+Вывести значение переменной calls на экран(в консоль).
+
+Пример результата выполнения программы:
+Пример выполняемого кода:
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+Вывод на консоль:
+(8, 'CAPYBARA', 'capybara')
+(10, 'ARMAGEDDON', 'armageddon')
+True
+False
+4
+Примечания:
+Для использования глобальной переменной внутри функции используйте оператор global.
+Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.'''
+
+calls = 0
+
+
+# str_1 = input('Введите слово: ')
+
+def count_calls():  # Подсчитывает вызов остальных функций
+    global calls
+    calls = calls + 1
+    return calls
+
+
+# print (count_calls())
+
+def string_info(str_1):  # Принимает строку и возвращает кортеж (len str, str.upper, str.lower).
+    global tuple_1
+    tuple_1 = len(str_1), str_1.upper(), str_1.lower()
+    count_calls()
+    return tuple_1
+
+
+# print (string_info())
+# print (tuple_1)
+
+def is_contains(str_1,
+                list_1):  # Принимает строку '' и список [] и возвращает True если строка находится в списке или False
+    list_lo = []
+    for list_2 in list_1:
+        list_lo.append(list_2.lower())
+        # print(list_lo)
+    count_calls()
+    # print (str_1.lower() in list_lo)
+    return str_1.lower() in list_lo
+    # if str_1.lower() in list_lo:
+    #     bool_1 = True
+    #     count_calls()
+    # elif str_1.lower() not in list_lo:
+    #     bool_1 = False
+    #     count_calls()
+    # return bool_1
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
