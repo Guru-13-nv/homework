37ac9faa1530b98c0ba2c59a9c649425c16db2a3
Guru-13-nv <gurmaks@mail.ru>
2024-09-13 18:03:16 +0300

    Удалены "лишние" комментарии

--- Module_3/module_3_4.py
+++ Module_3/module_3_4.py
@@ -12,19 +12,10 @@ def single_root_words(root_word, *other_words):
         other_words_lower.append(value_1.lower())
         value_lower = value_1.lower()
         root_word_lower = root_word.lower()
-        # if str(root_word_lower) in other_words_lower or root_word_lower.find(value_lower) > -1:
-        #     same_words.append(value_1) # Долго не мог сообразить, почему не выводит, а потом понял, что эл-та в спске
-        # нет а только часть элемента
         if value_lower.find(root_word) > -1 or root_word_lower.find(value_lower) > -1:
             same_words.append(value_1)
     return same_words
 
-
-# a='urban.fan@mail.ru'
-# b=['@', '.com', '.ru', '.net']
-# for c in b:#Цикл с использованием метода сравнения строк
-#     d=a.find(c)
-#     print(d)
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
