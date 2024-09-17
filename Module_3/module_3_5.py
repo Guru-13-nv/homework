3c9b671481483de6dba0ee7ceca26c194e4105bf
Guru-13-nv <gurmaks@mail.ru>
2024-09-17 11:56:23 +0300

    Добавлено условие, что бы соответвествовать замечанию преподавателя

--- Module_3/module_3_5.py
+++ Module_3/module_3_5.py
@@ -3,11 +3,22 @@
 
 def get_multiplied_digits(number):
     str_number = str(number)
+    len_str_number = len(str_number)-1
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return (first * get_multiplied_digits(int(str_number[1:])))
+    # if len(str_number) == 1:
+    #     return first
+    if int(str_number[-1]) != 0 and len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    elif int(str_number[-1]) == 0 and len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:len_str_number:]))
     else:
-        return (first)
+        return first
 
 result = get_multiplied_digits(40203)
+print(result)
+
+result = get_multiplied_digits(0)
+print(result)
+
+result = get_multiplied_digits(240)
 print(result)
