85b051421dc828ea8a4d32011b03ee52bda54b04
Guru-13-nv <gurmaks@mail.ru>
2024-09-17 18:08:40 +0300

    Добавлена лекция "Практика по функциям"

--- Module_3/module_3_Funk_lecture.py
+++ Module_3/module_3_Funk_lecture.py
@@ -118,5 +118,44 @@ def my_sum(n, *args, txt='Сумма чисел'):
     print(txt + ':', s)
 
 
-my_sum(1, 1, 2, 3, 4, 5) #Сумма чисел
-my_sum(2, 2,3,4,5,6, txt='Сумма квадратов')
+my_sum(1, 1, 2, 3, 4, 5)  # Сумма чисел
+my_sum(2, 2, 3, 4, 5, 6, txt='Сумма квадратов')
+
+'''Практика по функциям'''
+
+
+# Максимум в списке
+# Подсчёт чётных чисел в списке
+# Уникальный список
+
+def find_max(list_):  # Поиск максимального значеня
+    max_ = list_[0]
+    for i in list_:
+        if i > max_:
+            max_ = i
+    return max_
+
+
+print(find_max([1, 2, 1, 1, 0]))
+
+
+def count_event(list_):  # Подсчёт чётных чисел
+    counter = 0
+    for i in list_:
+        if i % 2 == 0 and i != 0:
+            counter += 1
+    return counter
+
+
+print(count_event([2, 2, 3, 4, 2, 1, 0]))
+
+
+def unique(list_):  # Уникальный список
+    new_list = []
+    for i in list_:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
+print(unique([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
