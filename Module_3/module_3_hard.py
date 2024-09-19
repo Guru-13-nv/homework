f752a0d12ec86e71cf961a169f283b2312df3bc7
Guru-13-nv <gurmaks@mail.ru>
2024-09-19 11:17:03 +0300

    Не рабочий вариант 2

--- Module_3/module_3_hard.py
+++ Module_3/module_3_hard.py
@@ -47,3 +47,43 @@ def calculate_structure_sum(ds):
 
 result = calculate_structure_sum(data_structure)
 print(result)
+
+#Вариант решения 2
+sum_result_2 = 0
+def calculate_structure_sum_2(*ds_2):
+    global sum_result_2
+    if ds_2 != 0:
+        for value_global in ds_2:
+            if isinstance(value_global, str) == True:
+                sum_result_2 = sum_result_2 + len(value_global)
+            elif isinstance(value_global, int) == True:
+                sum_result_2 = sum_result_2 + value_global
+            elif isinstance(value_global, tuple) == True and value_global != 0:
+                for value_internal in value_global:
+                    calculate_structure_sum_2(value_internal)
+            elif isinstance(value_global, list) == True  and value_global != 0:
+                for value_internal in value_global:
+                    calculate_structure_sum_2(value_internal)
+            elif isinstance(value_global, dict) == True and value_global != 0:
+                value_dict_list = list(value_global)
+                for value_dict_2 in value_dict_list:
+                    if isinstance(value_dict_2, dict) == True:
+                        calculate_structure_sum_2(list(value_dict_2))
+                    elif isinstance(value_dict_2, list) == True:
+                        calculate_structure_sum_2(value_dict_2)
+                    elif isinstance(value_dict_2, str) == True:
+                        calculate_structure_sum_2(value_dict_2)
+                        calculate_structure_sum_2(value_global[value_dict_2])
+    return sum_result_2
+
+result_2 = calculate_structure_sum_2(data_structure)
+print(result_2)
+
+a=''
+b=()
+c=[]
+d={'cube': 7, 'drum': 8}
+print(type(a),type(b),type(c),type(d))
+g=list(d)
+for i in g:
+    print(f'key={i}, val={d[i]}')
