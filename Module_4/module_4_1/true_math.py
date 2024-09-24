c77af22ab684d367db150721c9eb0a6a6a49c7bb
Guru-13-nv <gurmaks@mail.ru>
2024-09-24 17:28:54 +0300

    module_4_1

---
+++ Module_4/module_4_1/true_math.py
@@ -0,0 +1,10 @@
+import math
+from math import inf
+
+
+def divide(first, second):
+    if second == 0:
+        result_true = math.inf
+    else:
+        result_true = first / second
+    return result_true
