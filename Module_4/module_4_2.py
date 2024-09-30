cdf9a91a1ce585e8f2dc55cf534719e29bc66334
Guru-13-nv <gurmaks@mail.ru>
2024-09-30 12:51:19 +0300

    Правки

--- Module_4/module_4_2.py
+++ Module_4/module_4_2.py
@@ -1,8 +1,12 @@
 def test_function():
+    print('Функция test_function')
     def inner_function():
         print('Я в области видимости функции test_function')
+    if_=inner_function()
+    return if_
 
 tf_=test_function()
 print(tf_)
-if_=inner_function()
+if_=inner_function() #Можно закомментировать, т.к. что переменная, что функция находятся в локальном пространстве имен
+# функции test_function
 print(if_)
