a938a290a5c509944d0751c8af9b7933389f620e
Guru-13-nv <gurmaks@mail.ru>
2025-02-05 16:32:27 +0300

    Добавлено условие

--- Module_9/module_9_3.py
+++ Module_9/module_9_3.py
@@ -2,7 +2,7 @@
 first = ['Strings', 'Student', 'Computers'] #Сптроки из списка first
 second = ['Строка', 'Урбан', 'Компьютер'] #Сптроки из списка second
 
-first_result = (len(f) - len(s) for f, s in zip(first, second)) #Генераторная сборка первого списка с исполнением функции zip
+first_result = [len(f) - len(s) for f, s in zip(first, second) if len(f) - len(s) != 0] #Генераторная сборка первого списка с исполнением функции zip
 second_result = (len(first[i]) == len(second[i]) for i in range(len(first))) #Генераторная сборка второго списка с исполнением range и len
 
 print(list(first_result))
