4b00aa23190f0bb4f30692987d5b3b395292cd35
Guru-13-nv <gurmaks@mail.ru>
2025-02-05 16:28:16 +0300

    module_9_3.py

---
+++ Module_9/module 9_3.py
@@ -0,0 +1,9 @@
+# module 9_3
+first = ['Strings', 'Student', 'Computers'] #Сптроки из списка first
+second = ['Строка', 'Урбан', 'Компьютер'] #Сптроки из списка second
+
+first_result = (len(f) - len(s) for f, s in zip(first, second)) #Генераторная сборка первого списка с исполнением функции zip
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first))) #Генераторная сборка второго списка с исполнением range и len
+
+print(list(first_result))
+print(list(second_result))
